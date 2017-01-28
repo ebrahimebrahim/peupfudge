31853357cdf62c46fe87947be9c7e653b5b0877c
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2017-01-27 19:29:18 -0800

    can load ability tree from file in ability_tree.py

--- testing/ability_tree.py
+++ testing/ability_tree.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env python
 
-import random
+import random, re
 
 class Node(object):
-  def __init__(self, name, untrained_level=3, level=None):
+  def __init__(self, name, level=3, weight=None):
     self.name = name
-    self.untrained_level = untrained_level
-    self.level = level if level is not None else untrained_level
-    self.children_weights = []
+    self.level = level
+    self.children = []
+    self.weight = weight
     self.parent = None
-  def add_child(self, child_node, weight=1):
+  def add_child(self, child_node, weight=None):
     if not isinstance(child_node, self.__class__):
       raise TypeError("Child should be of type "+str(self.__class__)+".")
     child_node.parent = self
-    self.children_weights.append((child_node, weight))
-  def children(self):
-    return [c for c,w in self.children_weights]
-  def child_with_weight_by_name(self, name):
+    if weight:
+      child_node.weight = weight
+    elif not child_node.weight:
+      child_node.weight = 1
+    self.children.append(child_node)
+  def child(self, name):
     try:
-      return self.children_weights[[c.name for c,w in self.children_weights].index(name)]
+      return self.children[[c.name for c in self.children].index(name)]
     except ValueError:
       raise Exception(self.name+' has no child by the name '+name)
-  def child(self, name):
-    child, weight = self.child_with_weight_by_name(name)
-    return child
-  def weight_of(self, name):
-    child, weight = self.child_with_weight_by_name(name)
-    return weight
   def descendants(self):
-    return self.children()+[d for c in self.children() for d in c.descendants()]
+    return self.children+[d for c in self.children for d in c.descendants()]
   def descendant(self, name):
     descendants = self.descendants()
     try:
@@ -36,40 +32,66 @@ class Node(object):
     except ValueError:
       raise Exception(self.name+' has no descendant by the name '+name)
   def is_skill(self):
-    return not bool(self.children_weights)
-  def own_weight(self):
-    if self.parent is None:
-      raise Exception(self.name+' has no weight because it has no parent.')
-    return self.parent.weight_of(self.name)
+    return not bool(self.children)
   def __str__(self, indent=0):
-    own_label = indent*'  ' + '['+(str(self.own_weight()) if self.parent else '-')+'] ' + self.name + ": " + str(self.level)
-    children_labels = '\n'.join([c.__str__(indent+1) for c,w in self.children_weights])
+    own_label = indent*'  ' + '['+(str(self.weight) if self.weight else '-')+'] ' + self.name + ": " + str(self.level)
+    children_labels = '\n'.join([c.__str__(indent+1) for c in self.children])
     return own_label + ('\n' if not self.is_skill() else '') + children_labels
   def train(self, indirect = False):
     if not self.is_skill() and not indirect:
       raise Exception('Cannot directly train the attribute '+self.name+'.')
     self.level += 1
     if self.parent:
-      w = float(self.own_weight())
-      w_plus_n = sum(c.own_weight() for c in self.parent.children() if (c.level <= self.parent.level or c is self))
+      assert all(s.weight for s in self.parent.children) #all siblings should have a weight
+      w = float(self.weight)
+      w_plus_n = sum(s.weight for s in self.parent.children if (s.level <= self.parent.level or s is self))
       p = w / w_plus_n #probability that the parent will increase
       if random.random() < p:
         self.parent.train(indirect=True)
     
+def import_ability_tree(filename):
+  f = open(filename, 'r')
+  lines = f.readlines()
+  f.close()
+  def parse_line(line):
+    name_list = re.findall("(?<=]).+(?=:)", line)
+    weight_list = re.findall("(?<=\[).+(?=\])", line)
+    level_list = re.findall("(?<=:)[\d\s]+", line)
+    if any(len(l)!=1 for l in [name_list,weight_list,level_list]):
+      raise Exception("Could not parse the following line:\n"+line)
+    return name_list[0].strip(), weight_list[0].strip(), level_list[0].strip()
+  def indent_level(line):
+    initial_spaces_list = re.findall("^[ ]+", line)
+    if not initial_spaces_list:
+      return 0
+    assert(len(initial_spaces_list) == 1)
+    n = len(initial_spaces_list[0])
+    if n%2==1:
+      raise Exception("Encountered indentation issue while parsing the following line:\n"+line)
+    return n/2
+  def node_from_lines(lines):
+    assert(lines)
+    i = indent_level(lines[0])
+    assert(all(indent_level(line)>i for line in lines[1:]))
+    name, weight, level = parse_line(lines[0])
+    try:
+      weight_int = int(weight)
+    except ValueError:
+      weight_int = None
+    try:
+      level_int = int(level)
+    except ValueError:
+      raise Exception("Could not parse ability level from the following line:\n"+lines[0])
+    node = Node(name=name, level=level_int, weight=weight_int)
+    child_lines = []
+    for m in range(1,len(lines)):
+      child_lines.append(lines[m])
+      if m==len(lines)-1 or indent_level(lines[m+1])==i+1:
+        node.add_child(node_from_lines(child_lines))
+        child_lines=[]
+    assert(not child_lines)
+    return node
+  return node_from_lines(lines)
   
 
-eg = Node('level')
-eg.add_child(Node('mind'))
-eg.child('mind').add_child(Node('engineering', untrained_level=1))
-eg.child('mind').add_child(Node('literacy'), weight=2)
-eg.child('mind').add_child(Node('medicine', untrained_level=2))
-eg.child('mind').child('medicine').add_child(Node('wound care', untrained_level=2))
-eg.child('mind').child('medicine').add_child(Node('first aid'))
-eg.add_child(Node('body'))
-eg.child('body').add_child(Node('muscle strength'),weight=2)
-eg.descendant('muscle strength').add_child(Node('hand-to-hand combat'))
-eg.descendant('muscle strength').add_child(Node('hauling'))
-eg.child('body').add_child(Node('climbing', untrained_level=2))
-eg.child('body').add_child(Node('athletics'),weight=2)
-eg.descendant('athletics').add_child(Node('swimming', untrained_level=2))
-eg.descendant('athletics').add_child(Node('running', untrained_level=4))
+eg = import_ability_tree("example.tree")
