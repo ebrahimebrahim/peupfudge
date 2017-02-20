49a280d01fcc4a6954d68bef0f6b78116cc809a0
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2017-02-19 20:01:34 -0800

    update peuptest 2 data to focus on discount factor
    see docstrings for definition of this quantity
    this is the quantity that is supposed to be more or less constant
    if criterion 2 holds

--- python/peuptest.py
+++ python/peuptest.py
@@ -87,27 +87,31 @@ def test1_data(tree):
   """
   return {(skill.name,n) : test1_run(tree, skill.name, n) for skill in tree.skills() for n in range(1,max_skill_level) }
 
-def test2_run(tree, skill, s, t, o, r):
+def test2_run(tree, skill, s, t, o, r, r0):
   """Return data point for test criterion (2)
 
      Args:
        tree: tree to run test on
        skill: skill to train (as node)
-       s: starting level for skill
-       t: target level of skill
-       o: order of related skills to consider (see ability_tree.Node.related_skills to understand order)
-       r: level to train related skills up to beforehand
+       s:  starting level for skill
+       t:  target level of skill
+       o:  order of related skills to consider (see ability_tree.Node.related_skills to understand order)
+       r:  level to train related skills up to beforehand, starting from r0
+       r0: where to start related skills before training them up to r
 
      Returns:
        Expected xp cost of training skill from level s to level t,
-       given that related skills up to order o have first been trained up to level r
+       given that related skills up to order o have first been trained from r0 up to level r
 
-     Raises Exception:
-       If a related skill happens to start at a level higher than r
-
-     The way in which initial training of related skills is carried out is by taking the mean tree.
-     (see monte_carlo.mean_tree)
+     The way in which initial training of related skills is carried out is by taking the mean tree
+     (see monte_carlo.mean_tree).
+     Note that related skills of the specified order must first be *set* to a level of r0,
+     in order to train them all up to r from the same starting conditions.
+     While this may not reflect real peupfudge character development, it sufficies to provide
+     a coherent metric for test criterion 2.
   """
+  if r<r0:
+    raise Exception("When call test2_run you cannot train related skills to r="+str(r)+" starting from r0="+str(r0)+".")
   d_tree = copy.deepcopy(tree) # d stands for "dummy"
   d_skill = d_tree.descendant(skill.name)
   d_skill.level = s
@@ -115,27 +119,29 @@ def test2_run(tree, skill, s, t, o, r):
   for rel_skill in d_skill.related_skills(o):
     if rel_skill.name == d_skill.name:
       continue
-    if r - (rel_skill.level) < 0:
-      raise Exception("Warning: while running test 2, examining the skill \""+d_skill.name+"\", "
-                      "an attempt was made to train the related skill \""+rel_skill.name+"\" "
-                      "to level "+str(r)+". But it *starts* at level "+str(rel_skill.level)+".")
-    trainer += (r - rel_skill.level) * [rel_skill.name]
+    rel_skill.level = r0
+    trainer += (r - r0) * [rel_skill.name]
   d_tree = mean_tree(d_tree, run_trials(d_tree, trainer_from_sequence(trainer), num_trials=N))
+  if t-s==1:
+    return d_skill.cost_to_train()
   return expected_xpcost(d_tree, trainer_from_sequence((t-s)*[skill.name]), num_trials=N)
     
 def test2_data(tree, skill):
   """Return data set for test criterion (2)
 
      Given a tree and a skill in that tree (both Nodes), return a dict
-       (s,o,r) --> dc_dr
+       (o,s,r) --> DF
      where
-       dc_dr is the rate of change,
-         with respect to level of related skills,
-         of the expected xp cost of training the given skill by one level
-       s is the level at which to start the given skill
-       o is the order of relatedness of related skills
-         (see ability_tree.Node.related_skills docstring for meaning of order)
-       r is the level of related skills
+       DF is the "Discount Factor,"
+         which is defined to be DF = c_o(s,r+1) / c_o(s,r)
+     where
+       c_o(s,r) is the expected xp cost of training a skill at level s by one level
+         given that its related skills of order o have been trained up to r from r_start
+         (see definition of r_start in code)
+     s is the level at which to start the given skill
+     o is the order of relatedness of related skills
+       (see ability_tree.Node.related_skills docstring for meaning of order)
+     r is the level of related skills
 
      Perhaps the following is clearer:
        Fix a tree, and a skill in that tree.
@@ -143,32 +149,31 @@ def test2_data(tree, skill):
        The (expected) xp cost of training a skill by one level can be considered as a function c_o(s,r)
        of the skill's current level s, and the level r of its related skills of order o.
        (Assuming we only care to vary those things here, of course)
-       dc_dr is the estimated partial derivative of c_o with respect to r, evaluated at (s,r).
 
-     The range of (s,o,r)'s provided is small but reasonable:
+     The range of (o,s,r)'s provided is small but reasonable:
        o can be 1 or 2
-       s ranges from 1 to max_skill_level-1
-       r takes on some medium-low value, and some medium-high value.
+       s can take on a low or a high value
+       r ranges from r_start to 8
 
      Finally, a subtlety to heed:
       When we "set r" to some value to see it's effect on c_o(s,r), we are really *training* it
-      up to that value from whatever it starts at in the tree.
+      up to that value from whatever it starts at in the tree, after *setting* it to r_start. 
       This is essential for test criterion (2), because it is in the process of training related skills
       that the mechanic is allowed to take effect.
-      Note that this means that the results of test (2) do have some dependence on the starting levels of
-      skills related to the skill being tested.
   """
+  r_start = 2 # level to start related skills at before training them up to various r's
   o_range = [1,2]
-  def r_range(o):
-    min_r = max(s.level for s in skill.related_skills(o) if s != skill) #min allowable choice of r
-    max_r = max_skill_level #let's take this to be max allowable choice of r
-    return range(min_r, max_r+1)
-  def s_range(o):
-    min_s = 1
-    max_s = max_skill_level-1
-    return [int(round(f*(max_s-min_s)+min_s)) for f in [0.2,0.5,0.8]]
-  dc_dr = lambda s,t,o,r : test2_run(tree, skill, s,t,o,r+1)-test2_run(tree,skill, s,t,o,r)
-  return {(s,o,r):dc_dr(s,s+1,o,r) for o in os for r in rs_from_o(o) for s in ss}
+  r_range = range(r_start, max_skill_level+1)
+  min_s = 1
+  max_s = max_skill_level-1
+  s_range = [int(round(f*(max_s-min_s)+min_s)) for f in [0.2,0.8]]
+  DF = {}
+  for o in o_range:
+    for s in s_range:
+      c = {r:test2_run(tree,skill,s,s+1,o,r,r_start) for r in r_range}
+      for r in r_range[0:-1]:
+        DF[(o,s,r)] = float(c[r+1])/c[r]
+  return DF
 
 
 if __name__=="__main__":
