67bdc3623f8b128cc476cc5aeedb92b6d776dd44
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2017-03-16 21:05:54 -0700

    added body diagrams

--- make.sh
+++ make.sh
@@ -3,6 +3,11 @@ set -e
 
 python/gen_numbers.py
 python/gen_trees.py
+
+inkscape -D -z --file=svg/bp1.svg --export-pdf=bp1.pdf --export-latex
+inkscape -D -z --file=svg/bp2.svg --export-pdf=bp2.pdf --export-latex
+inkscape -D -z --file=svg/bp3.svg --export-pdf=bp3.pdf --export-latex
+
 pdflatex peupfudge.tex
 makeindex peupfudge.idx
 pdflatex peupfudge.tex
