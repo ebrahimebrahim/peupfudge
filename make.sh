14840d12e76f1d2e4bbf015e367d49e67a96a63a
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2017-03-16 21:35:08 -0700

    now using eps files converted from svg in manual

--- make.sh
+++ make.sh
@@ -4,10 +4,13 @@ set -e
 python/gen_numbers.py
 python/gen_trees.py
 
-inkscape -D -z --file=svg/bp1.svg --export-pdf=bp1.pdf --export-latex
-inkscape -D -z --file=svg/bp2.svg --export-pdf=bp2.pdf --export-latex
-inkscape -D -z --file=svg/bp3.svg --export-pdf=bp3.pdf --export-latex
-inkscape -D -z --file=svg/tb.svg  --export-pdf=tb.pdf  --export-latex
+cd svg
+inkscape -D -z --file=bp1.svg --export-eps=bp1.eps
+inkscape -D -z --file=bp2.svg --export-eps=bp2.eps
+inkscape -D -z --file=bp3.svg --export-eps=bp3.eps
+inkscape -D -z --file=bleeding.svg --export-eps=bleeding.eps
+inkscape -D -z --file=healing.svg --export-eps=healing.eps
+cd ..
 
 pdflatex peupfudge.tex
 makeindex peupfudge.idx
