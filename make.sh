bd224b3572e3d38f467c813462bc9adfc7ea5bf7
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2017-03-16 21:15:25 -0700

    broken commit. middle of trying to make tb.svg display in tex

--- make.sh
+++ make.sh
@@ -7,6 +7,7 @@ python/gen_trees.py
 inkscape -D -z --file=svg/bp1.svg --export-pdf=bp1.pdf --export-latex
 inkscape -D -z --file=svg/bp2.svg --export-pdf=bp2.pdf --export-latex
 inkscape -D -z --file=svg/bp3.svg --export-pdf=bp3.pdf --export-latex
+inkscape -D -z --file=svg/tb.svg  --export-pdf=tb.pdf  --export-latex
 
 pdflatex peupfudge.tex
 makeindex peupfudge.idx
