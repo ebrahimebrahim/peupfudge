d754758d3201d98b7ea287a905684787baf9667d
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2021-06-05 03:53:53 -0400

    bring current core peupfudge draft from google doc

--- manual/make.sh
+++ manual/make.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 set -e
 
-python/gen_numbers.py
-python/gen_trees.py
-
-cd svg
-inkscape -D -z --file=bp1.svg --export-eps=bp1.eps
-inkscape -D -z --file=bp2.svg --export-eps=bp2.eps
-inkscape -D -z --file=bp3.svg --export-eps=bp3.eps
-inkscape -D -z --file=bleeding.svg --export-eps=bleeding.eps
-inkscape -D -z --file=healing.svg --export-eps=healing.eps
-inkscape -D -z --file=tb.svg --export-eps=tb.eps
-inkscape -D -z --file=orders.svg --export-eps=orders.eps
-cd ..
-
-pdflatex peupfudge.tex
-makeindex peupfudge.idx
 pdflatex peupfudge.tex
