4f5eaeb33cc5b022f511969c8c9333745243049a
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2021-06-06 20:19:19 -0400

    bring probability reference appendix over into latex
    Ported over from the google drive folder

--- manual/python/make_ndf_data.py
+++ manual/python/make_ndf_data.py
@@ -21,10 +21,9 @@ def p(N,n):
   return num(N,n) / (3**N)
 
 
-ns = np.arange(-10,11,1)
-
+# Make plot
+ns = np.arange(-9,10,1)
 get_ps = lambda N : [p(N,n) for n in ns]
-
 plot_for_N = lambda N : plt.plot(ns, get_ps(N),label = str(N)+"dF")
 plot_for_N(2)
 plot_for_N(4)
@@ -34,3 +33,30 @@ plt.xticks(ns, labels=ns)
 plt.legend()
 plt.title("Probabilities when rolling NdF")
 plt.savefig("ndf_plot.pdf")
+
+
+# Make table
+cols = range(-5,6)
+rows = range(1,10)
+col_colored = 0
+row_colored = 4
+color_str = '\\cellcolor{blue!20}'
+with open("ndf_table.tex",'w') as f:
+  f.write('\\begin{tabular}{')
+  f.write('|' + '|'.join(['c']*(len(cols)+1)) + '|')
+  f.write('}\n\hline\n')
+  f.write(' '.join(f'& $\geq {c}$' for c in cols))
+  f.write('\\\\ \hline\n')
+  for r in rows:
+    f.write(f'${r}$dF ')
+    for c in cols:
+      f.write('& $')
+      cumulative_probability = sum(p(r,n) for n in range(c,r+1))
+      cumulative_probability_formatted = "{:.2f}".format(cumulative_probability)
+      f.write(cumulative_probability_formatted)
+      f.write('$ ')
+      if r==row_colored or c==col_colored:
+        f.write(color_str)
+    f.write('\\\\ \hline\n')
+  f.write('\\end{tabular}\n')
+
